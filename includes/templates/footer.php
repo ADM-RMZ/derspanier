a6e22480db687a846352687ed226731c6d94dbfe
Renzo <giancarlo.mendoza@gmail.com>
2022-05-31 15:51:37 +0200

    update htaccess

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -23,7 +23,8 @@
                 </nav>
             </div>
         </div>
-    </footer>    
+    </footer>   
+ 
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
     <script src="build/js/app.js"></script>
