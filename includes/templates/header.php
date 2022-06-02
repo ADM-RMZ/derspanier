c69d5851d9b7e3b00da190a556af4caf08ac8248
Renzo <giancarlo.mendoza@gmail.com>
2022-06-02 09:51:30 +0200

    update cookies

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -13,7 +13,18 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
     <link rel="stylesheet" href="build/css/app.css">
 </head>
-
+<div class="cookies" id="cookies">
+    <div class="container">
+        <div class="subcontainer">
+            <div class="cookies__contenido">
+              <p class="cookies__texto">Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.
+                <a class="cookies__enlace" href="/datenschutz">Details</a>
+              </p>
+              <button class="cookies-btn">OK, ich bin einverstanden.</button>
+            </div>
+        </div>
+    </div>
+</div>
 <body>
 
     <header class="header ">
